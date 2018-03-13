f911cbdb2bac20a52288855e15fc961e08d10653
Grigory Skvortsov <skvortsovg@yandex.ru>
2018-03-13 23:52:53 +0300

    init and solved srp from solid-master1

---
+++ solid-master1/src/com/github/bsferreira/solid/srp/violation/FuelStation.java
@@ -0,0 +1,7 @@
+package com.github.bsferreira.solid.srp.violation;
+
+public class FuelStation {
+    public void reFuel(final Vehicle vehicle) {
+        vechicle.setRemainingFuel(vechicle.getMaxFuel())
+    }
+}
